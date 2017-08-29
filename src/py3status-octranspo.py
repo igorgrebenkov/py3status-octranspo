3686eb7dba6f83f9fb9be6237b7a4dbe3263c016
Igor Grebenkov <igor.grebenkov@gmail.com>
2017-08-29 03:10:16 -0400

    Can make basic JSON requests using OCTranspo API
    - Current example fetches some information about the trips at a stop and
      displays some information from the JSON result

---
+++ src/py3status-octranspo.py
@@ -0,0 +1,19 @@
+import json, requests
+
+def main():
+    payload = { 'appID': '8856da69',
+                'apiKey': '63f69bfe2ea5e6e8b12511000f01d73c', 
+                'routeNo': '61',
+                'stopNo': '6908',
+                'format': 'JSON' }
+    r = requests.get('https://api.octranspo1.com/v1.2/GetNextTripsForStop', params = payload)
+    print(r.url)
+    data = r.json() 
+    print("StopNo: " + data['GetNextTripsForStopResult']['StopNo'])
+    print("Stop Label: " + data['GetNextTripsForStopResult']['StopLabel'])
+    print("RouteNo: " + str(data['GetNextTripsForStopResult']['Route']['RouteDirection']['RouteNo']))
+    print("Trip 1: " + str(data['GetNextTripsForStopResult']['Route']['RouteDirection']['Trips']['Trip'][0]['AdjustedScheduleTime']))
+    print("Trip 2: " + str(data['GetNextTripsForStopResult']['Route']['RouteDirection']['Trips']['Trip'][1]['AdjustedScheduleTime']))
+    print("Trip 3: " + str(data['GetNextTripsForStopResult']['Route']['RouteDirection']['Trips']['Trip'][2]['AdjustedScheduleTime']))
+if __name__ == "__main__": main()
+
