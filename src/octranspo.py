4c1e007fe2275b8e1d5f4e8a130ab757ecb68744
Igor Grebenkov <igor.grebenkov@gmail.com>
2017-08-29 10:14:19 -0400

    read appID/apiKey from file to obfuscate from main .py file

--- src/octranspo.py
+++ src/octranspo.py
@@ -3,13 +3,13 @@
 
 __author__ = "Igor Grebenkov"
 
-import json, requests
+import json, requests, os
 
 # Fetches a JSON response from the OCTranspo API
-def getJSON(routeNo, stopNo):
+def getJSON(appID, apiKey, routeNo, stopNo):
     payload = { 
-                'appID': '8856da69',
-                'apiKey': '63f69bfe2ea5e6e8b12511000f01d73c', 
+                'appID': appID,
+                'apiKey': apiKey, 
                 'routeNo': routeNo,
                 'stopNo': stopNo,
                 'format': 'JSON' 
@@ -80,9 +80,15 @@ class Py3status:
     def __init__(self):
         self.button = None
         self.button_down = False
-
+    
+        path = os.path.abspath('git/py3status-octranspo/login')
+    
+        self.login_file = open(path)
+        self.appID = self.login_file.readline().rstrip('\n')
+        self.apiKey = self.login_file.readline().rstrip('\n')
+    
     def OCTranspo(self):
-        data = getJSON(self.routeNo, self.stopNo)
+        data = getJSON(self.appID, self.apiKey, self.routeNo, self.stopNo)
         result = parseData(data, self.direction)
 
         # Assign color based on trip time relative to low_threshold 
@@ -176,46 +182,3 @@ class Py3status:
 
     def on_click(self, event):
         self.button = event['button']
-
-
-
-# """
-# Example module that demonstrates status string placeholders
-
-# Configuration parameters:
-#     format: Initial format to use
-#         (default 'Click me')
-#     format_clicked: Display format to use when we are clicked
-#         (default 'You pressed button {button}')
-
-# Format placeholders:
-#     {button} The button that was pressed
-# """
-
-
-# class Py3status:
-#     format = 'Click me'
-#     format_clicked = 'You pressed button {button}'
-
-#     def __init__(self):
-#         self.button = None
-
-#     def click_info(self):
-#         if self.button:
-#             data = {'button': self.button}
-#             full_text = self.py3.safe_format(self.format_clicked, data)
-#         else:
-#             full_text = self.format
-
-#         return {
-#             'full_text': full_text,
-#             'cached_until': self.py3.CACHE_FOREVER
-#         }
-
-#     def on_click(self, event):
-#         """
-#         event will be a dict like
-#         {'y': 13, 'x': 1737, 'button': 1, 'name': 'example', 'instance': 'first'}
-#         """
-#         self.button = event['button']
-#         # Our modules update methods will get called automatically.
