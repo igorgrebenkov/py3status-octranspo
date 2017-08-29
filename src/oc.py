edebbf5f674165baa999d2aee444c4bd18dd27b5
Igor Grebenkov <igor.grebenkov@gmail.com>
2017-08-29 05:32:18 -0400

    Fix to support uni & bidir. trips + config options
      - added 'routeNo' and 'stopNo' options to specify a route/stop
      - added 'format' option to specify format of the resulting string
      - new configuration option 'direction' -> can be 'east' or 'west'
      - check the JSON to see if data...['RouteDirection'] is a list
        - if it is, we can index the direction we want using 'direction'
          which is translated to a boolean
        - else, we just grab the route
      - we now also grab the route direction and use the first char to
        display the direction in the bar

--- src/oc.py
+++ src/oc.py
@@ -1,29 +1,64 @@
 import json, requests
 
 class Py3status:
-    format = '{routeNo} ({trip1} / {trip2} / {trip3})'
     
-    def __init__(self):
-        
+    format = '{routeNo} {direction} - {trip1} / {trip2} / {trip3}'
+    routeNo = '0' 
+    stopNo = '0'
+    direction = ''
+    
+    def octranspo(self):
         payload = { 'appID': '8856da69',
                     'apiKey': '63f69bfe2ea5e6e8b12511000f01d73c', 
-                    'routeNo': '61',
-                    'stopNo': '6908',
+                    'routeNo': self.routeNo,
+                    'stopNo': self.stopNo,
                     'format': 'JSON' }
         
         r = requests.get('https://api.octranspo1.com/v1.2/GetNextTripsForStop', params = payload)
         data = r.json()
+        
+        if self.direction == 'east':
+            dirNo = 0
+        elif self.direction == 'west':
+            dirNo = 1
 
         self.stopNo = data['GetNextTripsForStopResult']['StopNo']
         self.stopLabel = data['GetNextTripsForStopResult']['StopLabel']
-        self.routeNo = str(data['GetNextTripsForStopResult']['Route']['RouteDirection']['RouteNo'])
-        self.trip1 = str(data['GetNextTripsForStopResult']['Route']['RouteDirection']['Trips']['Trip'][0]['AdjustedScheduleTime'])
-        self.trip2 = str(data['GetNextTripsForStopResult']['Route']['RouteDirection']['Trips']['Trip'][1]['AdjustedScheduleTime'])
-        self.trip3 = str(data['GetNextTripsForStopResult']['Route']['RouteDirection']['Trips']['Trip'][2]['AdjustedScheduleTime'])
+        
+        if type(data['GetNextTripsForStopResult']['Route']['RouteDirection']) is list:
+            self.routeNo = str(
+                    data['GetNextTripsForStopResult']['Route']['RouteDirection'][dirNo]['RouteNo'])
+
+            self.routeDir = data['GetNextTripsForStopResult']['Route']['RouteDirection'][dirNo]['Direction']
+
+            self.trip1 = str(
+                    data['GetNextTripsForStopResult']['Route']['RouteDirection'][dirNo]['Trips']['Trip'][0]['AdjustedScheduleTime'])
+
+            self.trip2 = str(
+                    data['GetNextTripsForStopResult']['Route']['RouteDirection'][dirNo]['Trips']['Trip'][1]['AdjustedScheduleTime'])
+
+            self.trip3 = str(
+                    data['GetNextTripsForStopResult']['Route']['RouteDirection'][dirNo]['Trips']['Trip'][2]['AdjustedScheduleTime'])
+        else:
+            self.routeNo = str(
+                    data['GetNextTripsForStopResult']['Route']['RouteDirection']['RouteNo'])
+
+            self.routeDir = data['GetNextTripsForStopResult']['Route']['RouteDirection']['Direction']
+
+            self.trip1 = str(
+                    data['GetNextTripsForStopResult']['Route']['RouteDirection']['Trips']['Trip'][0]['AdjustedScheduleTime'])
+
+            self.trip2 = str(
+                    data['GetNextTripsForStopResult']['Route']['RouteDirection']['Trips']['Trip'][1]['AdjustedScheduleTime'])
+
+            self.trip3 = str(
+                    data['GetNextTripsForStopResult']['Route']['RouteDirection']['Trips']['Trip'][2]['AdjustedScheduleTime'])
+            
         self.full_text = self.routeNo + " (" + self.trip1 + " / " + self.trip2 + " / " + self.trip3 + ")"
-    
-    def octranspo(self):
-        full_text = self.py3.safe_format(self.format, {'routeNo': self.routeNo, 'trip1': self.trip1, 'trip2': self.trip2, 'trip3': self.trip3})
+        
+        full_text = self.py3.safe_format(self.format, 
+                {'routeNo': self.routeNo, 'direction': self.routeDir[0], 'trip1': self.trip1, 'trip2': self.trip2, 'trip3': self.trip3})
+
         return {
                 'full_text': full_text,
                 'cached_until': self.py3.CACHE_FOREVER
--- src/py3status-octranspo.py
+++ src/py3status-octranspo.py
@@ -1,12 +1,16 @@
 import json, requests
 
 def main():
+    stopNo = '61'
+    
     payload = { 'appID': '8856da69',
                 'apiKey': '63f69bfe2ea5e6e8b12511000f01d73c', 
-                'routeNo': '61',
-                'stopNo': '6908',
+                'routeNo': '95',
+                'stopNo': '3000',
                 'format': 'JSON' }
+
     r = requests.get('https://api.octranspo1.com/v1.2/GetNextTripsForStop', params = payload)
+    
     print(r.url)
     data = r.json() 
     print("StopNo: " + data['GetNextTripsForStopResult']['StopNo'])
@@ -14,6 +18,7 @@ def main():
     print("RouteNo: " + str(data['GetNextTripsForStopResult']['Route']['RouteDirection']['RouteNo']))
     print("Trip 1: " + str(data['GetNextTripsForStopResult']['Route']['RouteDirection']['Trips']['Trip'][0]['AdjustedScheduleTime']))
     print("Trip 2: " + str(data['GetNextTripsForStopResult']['Route']['RouteDirection']['Trips']['Trip'][1]['AdjustedScheduleTime']))
+
     print("Trip 3: " + str(data['GetNextTripsForStopResult']['Route']['RouteDirection']['Trips']['Trip'][2]['AdjustedScheduleTime']))
 if __name__ == "__main__": main()
 
